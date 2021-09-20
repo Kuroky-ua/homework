10e5fd822b11f197235de7e7f1813fbf67e1d81a
Serg <chabanserg@gmail.com>
2021-09-20 19:53:14 +0300

    Feat: add radium

--- my-app/src/Car/Car.js
+++ my-app/src/Car/Car.js
@@ -1,5 +1,7 @@
+import Radium from "radium";
 import './Car.css'
-export default props =>  {
+
+const Car = props =>  {
     const inputClasses = ['input'
 
     ]
@@ -11,16 +13,27 @@ export default props =>  {
     if (props.name.length > 4 ) {
         inputClasses.push('bold')
     }
+    const style = {
+        border: '1px solid #ccc',
+        boxShadow:'0 4px 5px 0 rgba(0, 0, 0. .14',
+        ':hover': {
+            border: '1px solid #aaa',
+            boxShadow: '0 4px 15px 0 rgba(0, 0, 0, .25)',
+            cursor:'pointer'
+        }
+    }
 
     return(
-    <div className="Car">
-        <h3> Car name : {props.name}</h3>
-        <p> Year: <strong>{props.year}</strong></p>
-        <input type="text" onChange={props.onChangeName}
-               value={props.name}
-               className={inputClasses.join(' ')}
-        />
-        <button onClick={props.onDelete}>Delete</button>
-    </div>
-)
+        <div className="Car" style={style}>
+            <h3> Car name : {props.name}</h3>
+            <p> Year: <strong>{props.year}</strong></p>
+            <input type="text" onChange={props.onChangeName}
+                   value={props.name}
+                   className={inputClasses.join(' ')}
+            />
+            <button onClick={props.onDelete}>Delete</button>
+        </div>
+    )
 }
+
+export default Radium (Car)
