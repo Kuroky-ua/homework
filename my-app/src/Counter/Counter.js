7f282ae1449101c11cd24965141ad6ce8f837a26
Serg <chabanserg@gmail.com>
2021-09-26 09:37:44 +0300

    fix: correct change of state

--- my-app/src/Counter/Counter.js
+++ my-app/src/Counter/Counter.js
@@ -7,8 +7,13 @@ export default class Counter extends Component {
         counter:0
     }
     addCounter = () => {
-        this.setState({
-            counter:this.state.counter +1
+        // this.setState({
+        //     counter: this.state.counter +1
+        // })
+        this.setState( (prevState) =>{
+         return {
+             counter: prevState.counter + 1
+         }
         })
     }
     render() {
